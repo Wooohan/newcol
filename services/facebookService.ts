7d72ed975471f5ee0a1e35547107b107f4b79c3a
Wooohan <wooohan3@gmail.com>
2026-08-16 04:37:55 +0500

    Update Facebook App ID in facebookService.ts

--- services/facebookService.ts
+++ services/facebookService.ts
@@ -4,7 +4,7 @@ import { FacebookPage, Conversation, Message, ConversationStatus } from '../type
 /**
  * Meta App ID: 1148755260666274
  */
-const FB_APP_ID: string = '987921680673147'; 
+const FB_APP_ID: string = '907131161783008'; 
 
 let sdkPromise: Promise<void> | null = null;
 
